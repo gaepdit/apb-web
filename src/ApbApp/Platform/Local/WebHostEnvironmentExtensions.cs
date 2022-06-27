d928c6518651ea690aca4af03576a62ebe2bba97
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-06-27 10:48:26 -0400

    Update launch configurations/environment

---
+++ src/ApbApp/Platform/Local/WebHostEnvironmentExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace ApbApp.Platform.Local;
+
+internal static class WebHostEnvironmentExtensions
+{
+    internal static bool IsLocalEnv(this IWebHostEnvironment env) => env.IsEnvironment("Local");
+}
--- src/ApbApp/Program.cs
+++ src/ApbApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ApbApp.Data;
+using ApbApp.Platform.Local;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment() && !app.Environment.IsLocalEnv())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
